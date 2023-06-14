908b3cb77597ea0965b3949df0eceff664fc9dd7
na2sime <nassime.abdiou@icloud.com>
2023-06-14 14:41:27 +0200

    finished

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,58 +1,42 @@
 import "./contactform.scss"
+import {RiCloseLine} from "react-icons/ri";
 
 function ContactForm({setIsOpen}) {
-
     return (
         <>
             <div className={"darkBG"} onClick={() => setIsOpen(false)}/>
-            <div className={"contactor"}>
-                <div className="background">
-                    <div className="container">
-                        <div className="screen">
-                            <div className="screen-header">
-                                <div className="screen-header-left">
-                                    <div className="screen-header-button close"></div>
-                                    <div className="screen-header-button maximize"></div>
-                                    <div className="screen-header-button minimize"></div>
-                                </div>
-                                <div className="screen-header-right">
-                                    <div className="screen-header-ellipsis"></div>
-                                    <div className="screen-header-ellipsis"></div>
-                                    <div className="screen-header-ellipsis"></div>
-                                </div>
+            <div className={"centered"}>
+                <div className={"modal"}>
+                    <div className={"modalHeader"}>
+                        <h5 className={"heading"}>Contact me ?</h5>
+                    </div>
+                    <button className={"closeBtn"} onClick={() => setIsOpen(false)}>
+                        <RiCloseLine style={{marginBottom: "-3px"}}/>
+                    </button>
+                    <div className={"modalContent"}>
+                        <form>
+                            <div className={"formGroup"}>
+                                <label htmlFor={"name"}>NAME</label>
+                                <input type={"text"} id={"name"} placeholder={"Enter your name"}/>
                             </div>
-                            <div className="screen-body">
-                                <div className="screen-body-item left">
-                                    <div className="app-title">
-                                        <span>CONTACT</span>
-                                        <span>ME</span>
-                                    </div>
-                                    <div className="app-contact">CONTACT INFO : +33 6 52 12 41 86</div>
-                                </div>
-                                <div className="screen-body-item">
-                                    <div className="app-form">
-                                        <div className="app-form-group">
-                                            <input className="app-form-control" placeholder="NAME"/>
-                                        </div>
-                                        <div className="app-form-group">
-                                            <input className="app-form-control" placeholder="EMAIL"/>
-                                        </div>
-                                        <div className="app-form-group">
-                                            <input className="app-form-control" placeholder="CONTACT NO"/>
-                                        </div>
-                                        <div className="app-form-group message">
-                                            <input className="app-form-control" placeholder="MESSAGE"/>
-                                        </div>
-                                        <div className="app-form-group buttons">
-                                            <button className="app-form-button"
-                                                    onClick={() => setIsOpen(false)}>CANCEL
-                                            </button>
-                                            <button className="app-form-button" onSubmit={() => setIsOpen(false)}>SEND
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
+                            <div className={"formGroup"}>
+                                <label htmlFor={"email"}>EMAIL</label>
+                                <input type={"email"} id={"email"} placeholder={"Enter your email"}/>
                             </div>
+                            <div className={"formGroup"}>
+                                <label htmlFor={"message"}>MESSAGE</label>
+                                <textarea id={"message"} placeholder={"Enter your message"}/>
+                            </div>
+                        </form>
+                    </div>
+                    <div className={"modalActions"}>
+                        <div className={"actionsContainer"}>
+                            <button className={"cancelBtn"} onClick={() => setIsOpen(false)}>
+                                Cancel
+                            </button>
+                            <button className={"sendBtn"} onClick={() => setIsOpen(false)}>
+                                Send
+                            </button>
                         </div>
                     </div>
                 </div>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,17 @@
 import "./navbar.scss"
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
 import ContactForm from "../contact/ContactForm";
 
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        if(isOpen){
+            document.body.style.overflow = 'hidden';
+            return ()=> document.body.style.overflow = 'unset';
+        }
+    }, [isOpen]);
 
     return (
         <div className={"navbar"}>
